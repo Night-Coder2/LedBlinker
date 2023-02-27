a1dbe120d853d3c167adb184ed772ab8ba41c218
Phoenixlegen <sudhircks@gmail.com>
2023-02-27 18:29:51 +0530

    module

---
+++ include/LedBlinker.h
@@ -0,0 +1,34 @@
+//
+// Created by Sudhir on 2/27/2023.
+//
+
+#ifndef PICOPROJECTS_LEDBLINKER_H
+#define PICOPROJECTS_LEDBLINKER_H
+
+#include <pico/stdlib.h>
+#include <stdint.h>
+#include <Led.h>
+#include <Millis.h>
+
+class LedBlinker {
+private:
+    Led led;
+    unsigned long lastTimeBlinked;
+    unsigned long blinkDelay;
+
+    void toggleLed();
+public:
+    LedBlinker() {}
+    LedBlinker(Led &led);
+    LedBlinker(Led &led, unsigned long blinkDelay);
+
+    void initLed();
+
+    void update();
+
+    unsigned long getBlinkDelay();
+    void setBlinkDelay(unsigned long blinkDelay);
+};
+
+
+#endif //PICOPROJECTS_LEDBLINKER_H
